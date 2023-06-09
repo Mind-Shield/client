73793943d8bd40dbb50d31e37dd6bf8fe314a823
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2023-06-09 16:14:41 -0300

    feat(adminHome,cardClass) :sparkles:

---
+++ src/components/CardClass/CardClass.jsx
@@ -0,0 +1,24 @@
+import './CardClass.css'
+
+function CardClass(){
+
+    return(
+        <div className='body-cardclass'>
+            <div className='item-cardclass'>
+                <div className='name-cardclass'>
+                    <p>s01-T3b-a23</p>
+                </div>
+                
+            </div>
+            <div className='item-cardclass'>
+                <p>s01-T3b-a23</p>
+            </div>
+            
+            
+        </div>
+    );
+
+
+};
+
+export default CardClass
--- src/pages/admin/AdminHome/AdminHome.jsx
+++ src/pages/admin/AdminHome/AdminHome.jsx
@@ -1,12 +1,30 @@
 import './AdminHome.css'
 import NavBar from '../../../components/NavBar/NavBar';
+import CardClass from '../../../components/CardClass/CardClass';
+import { Link } from 'react-router-dom';
 
 function AdminHome(){
 
     return(
         <div className='body-admin-home'>
             <div className='header'>
-            <NavBar></NavBar>
+                <NavBar></NavBar>
+            </div>
+            <div className='content-home'>
+                <div className='title-home'>
+                    <h1>TURMAS</h1>
+                </div>
+                <div className='card-home-div'>
+                    <CardClass></CardClass>
+                </div>
+                <div className='title-home'>
+                    <h1>TESTES</h1>
+                </div>
+                <div className='button-addquestion-div'>
+                    <Link className='link-addquestion'>
+                        <button className='button-addquestion'>Adicionar nova pergunta ao jogo</button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
