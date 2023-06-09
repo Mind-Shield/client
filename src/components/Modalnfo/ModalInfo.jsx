db3e5918a04962bd506e86e33a40695cbcc05501
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2023-06-09 13:55:40 -0300

    feat(ModalInfo):bookmark: : static student journey

--- src/components/Modalnfo/ModalInfo.jsx
+++ src/components/Modalnfo/ModalInfo.jsx
@@ -1,32 +1,36 @@
 import './ModalInfo.css'
 import { Link } from 'react-router-dom';
+import React from 'react';
+import Modal from 'react-modal';
 
 
 
-function ModalInfo(){
+function ModalInfo({ isOpen, closeModal }){
 
     return(
-        <div className='body-info'>
-            <div className='modal-info'>
-                <div className='title-info'>
-                    <h1>Sugestões norteadoras</h1>
-                </div>
-                <div className='text-info'> 
-                    <ul>
-                        <li>Quais sentimentos você sentiria?  ex: tranquilidade, paciência, raiva, tristeza...</li>
-                        <li>Quais ações você tomaria a partir disso?</li>
-                        <li>O que você falaria?</li>
-                    </ul>
-                </div>
-                <div className='button-close-info-div'>
-                    <Link className='link-close-info'>
-                        <button className='button-close-info'>fechar</button>
-                    </Link>
+        <Modal className="Modal-style" isOpen={isOpen} onRequestClose={closeModal}>
+            <div className='body-info'>
+                <div className='modal-info'>
+                    <div className='title-info'>
+                        <h1>Sugestões norteadoras</h1>
+                    </div>
+                    <div className='text-info'> 
+                        <ul>
+                            <li>Quais sentimentos você sentiria?  ex: tranquilidade, paciência, raiva, tristeza...</li>
+                            <li>Quais ações você tomaria a partir disso?</li>
+                            <li>O que você falaria?</li>
+                        </ul>
+                    </div>
+                    <div className='button-close-info-div'>
+                        <Link className='link-close-info'>
+                            <button onClick={closeModal} className='button-close-info'>fechar</button>
+                        </Link>
+                    </div>
+                    
                 </div>
                 
             </div>
-            
-        </div>
+        </Modal>
     );
 };
 
