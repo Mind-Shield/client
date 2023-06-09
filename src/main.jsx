c578e7939b7c956c341651f22a44ffddc7a5f61d
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2023-06-09 00:24:59 -0300

    feat(StartPage):sparkles:

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import StartPage from './pages/StartPage/StartPage.jsx';
 
+const router = createBrowserRouter([
+ 
+  {
+    path:"/StartPage",
+    element:<StartPage/>
+  },
+
+
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
---
+++ src/pages/StartPage/StartPage.jsx
@@ -0,0 +1,19 @@
+import './StartPage.css'
+
+
+
+function StartPage(){
+
+    return(
+        <div className='body-startpage'>
+            <div className='title-startpage'>
+                <h1>Mind Shild</h1>
+            </div>
+            <div className='button-play-div'>
+                <button className='button-play'>PLAY</button>
+            </div>
+        </div>
+    );
+};
+
+export default StartPage;
